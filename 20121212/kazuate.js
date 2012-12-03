71c4e7a84021b6aefbae7b29fc19589ad3643d4d
webcrafts <webcrafts098@gmail.com>
2012-12-03 16:22:13 +0900

    update

--- 20121212/kazuate.js
+++ 20121212/kazuate.js
@@ -4,16 +4,18 @@
   2012/12/12
  */
 
-var foo = Math.floor(Math.random()*100)+1;  // コンピュータ側の数値生成（1〜100）
-
-while(myname != yourname){
-  if(yourname != ""){
-
-    document.write("名前が違うよ<br />");
+var PcNum = Math.floor(Math.random()*100)+1;  // コンピュータ側の数値生成（1〜100）
+var MyNum;
+while(PcNum != MyNum){
+  MyNum = prompt("値はいくつですか？");
+  if(MyNum > PcNum){
+    document.write("大きすぎる<br />");
+  }else if(MyNum < PcNum){
+    document.write("小さいっす<br />");
   }
-  yourname = prompt("あなたのお名前はなんですか？");
 }
-document.write("こんにちは、" + yourname +"さん");
+
+document.write("ぴったり<br />");
 
 /*
   20121203  とりあえず動けばいいってくらいのコードなので、また時間のある時にでも書き加える
